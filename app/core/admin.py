02d587cb128e67e3f4d574bb2a17b27cbeb00b48
MateuszK <klusowskimat@gmail.com>
2023-02-12 22:43:23 +0100

    registration of models to the admin site.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,58 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import Plan, Thumbnail
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ('id',)
+    list_display = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name', 'plan')}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2')
+        }),
+    )
+    search_fields = ('email',)
+    search_help_text = 'Search by email'
+
+
+class ThumbnailInline(admin.TabularInline):
+    model = Plan.thumbnails.through
+    extra = 1
+    verbose_name = "thumbnail"
+
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'expired_link', 'original_image')
+    list_filter = ("expired_link", 'original_image')
+    inlines = (ThumbnailInline,)
+    exclude = ('thumbnails',)
+    fieldsets = (
+        (None, {"fields": ("name",)}),
+        (
+            _('Expired link'),
+            {'classes': ('collapse',), 'fields': ('expired_link',)},
+        ),
+        (
+            _('Original image'),
+            {'classes': ('collapse',), 'fields': ('original_image',)},
+        ),
+    )
+
+
+admin.site.unregister(Group)
+admin.site.register(get_user_model(), UserAdmin)
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(Thumbnail)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,93 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from core.models import Plan, Thumbnail
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@test.com', name='admin', password='testadminpassword') # noqa
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com', password='testpassword', name='testusername') # noqa
+        self.thumbnail = Thumbnail.objects.create(value=100)
+        self.plan = Plan.objects.create(name='Plan')
+        self.plan.thumbnails.add(self.thumbnail)
+
+    def test_users_listed(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.email)
+
+    def test_change_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_user_page(self):
+        url = reverse('admin:core_user_delete', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_thumbnail_listed(self):
+        url = reverse('admin:core_thumbnail_changelist')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, str(self.thumbnail.value))
+
+    def test_change_thumbnail_page(self):
+        url = reverse('admin:core_thumbnail_change', args=[self.thumbnail.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_thumbnail_page(self):
+        url = reverse('admin:core_thumbnail_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_thumbnail_page(self):
+        url = reverse('admin:core_thumbnail_delete', args=[self.thumbnail.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_plan_listed(self):
+        url = reverse('admin:core_plan_changelist')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.plan.name)
+
+    def test_change_plan_page(self):
+        url = reverse('admin:core_plan_change', args=[self.plan.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_plan_page(self):
+        url = reverse('admin:core_plan_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_plan_page(self):
+        url = reverse('admin:core_plan_delete', args=[self.plan.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
